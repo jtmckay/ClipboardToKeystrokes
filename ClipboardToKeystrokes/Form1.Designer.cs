4167443cd929f118f847cfd5414a3120d697d948
jtmckay <jtmckay@gmail.com>
2018-11-16 10:18:06 -0700

    Initial commit

---
+++ ClipboardToKeystrokes/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ClipboardToKeystrokes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.batchSizeTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.delayTB = new System.Windows.Forms.TextBox();
+            this.cb_replaceCarrage = new System.Windows.Forms.CheckBox();
+            this.cb_shiftReturn = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 92);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(553, 358);
+            this.textBox1.TabIndex = 0;
+            // 
+            // batchSizeTB
+            // 
+            this.batchSizeTB.Location = new System.Drawing.Point(101, 16);
+            this.batchSizeTB.Name = "batchSizeTB";
+            this.batchSizeTB.Size = new System.Drawing.Size(100, 26);
+            this.batchSizeTB.TabIndex = 1;
+            this.batchSizeTB.Text = "1000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Batch size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(256, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Delay Between Batches";
+            // 
+            // delayTB
+            // 
+            this.delayTB.Location = new System.Drawing.Point(441, 16);
+            this.delayTB.Name = "delayTB";
+            this.delayTB.Size = new System.Drawing.Size(100, 26);
+            this.delayTB.TabIndex = 4;
+            this.delayTB.Text = "2000";
+            // 
+            // cb_replaceCarrage
+            // 
+            this.cb_replaceCarrage.AutoSize = true;
+            this.cb_replaceCarrage.Checked = true;
+            this.cb_replaceCarrage.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cb_replaceCarrage.Location = new System.Drawing.Point(13, 52);
+            this.cb_replaceCarrage.Name = "cb_replaceCarrage";
+            this.cb_replaceCarrage.Size = new System.Drawing.Size(169, 24);
+            this.cb_replaceCarrage.TabIndex = 5;
+            this.cb_replaceCarrage.Text = "Replace \\r\\n with \\n";
+            this.cb_replaceCarrage.UseVisualStyleBackColor = true;
+            // 
+            // cb_shiftReturn
+            // 
+            this.cb_shiftReturn.AutoSize = true;
+            this.cb_shiftReturn.Checked = true;
+            this.cb_shiftReturn.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cb_shiftReturn.Location = new System.Drawing.Point(260, 51);
+            this.cb_shiftReturn.Name = "cb_shiftReturn";
+            this.cb_shiftReturn.Size = new System.Drawing.Size(150, 24);
+            this.cb_shiftReturn.TabIndex = 6;
+            this.cb_shiftReturn.Text = "Return with shift";
+            this.cb_shiftReturn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 450);
+            this.Controls.Add(this.cb_shiftReturn);
+            this.Controls.Add(this.cb_replaceCarrage);
+            this.Controls.Add(this.delayTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.batchSizeTB);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Clipboard to Keystrokes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox batchSizeTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox delayTB;
+        private System.Windows.Forms.CheckBox cb_replaceCarrage;
+        private System.Windows.Forms.CheckBox cb_shiftReturn;
+    }
+}
+
---
+++ ClipboardToKeystrokes/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ClipboardToKeystrokes
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+        [DllImport("user32.dll")]
+        private static extern short VkKeyScan(char ch);
+
+        [DllImport("User32.dll", SetLastError = true)]
+        private static extern uint SendInput(int nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] inputs, int cbSize);
+
+        private GlobalHotkey ghk;
+
+        const uint KEYEVENTF_KEYUP = 0x0002;
+        const uint KEYEVENTF_EXTENDEDKEY = 0x0001;
+        public class Modifiers
+        {
+            //modifiers
+            public const int NOMOD = 0x0000;
+            public const int ALT = 0x0001;
+            public const int CTRL = 0x0002;
+            public const int SHIFT = 0x0004;
+            public const int WIN = 0x0008;
+        }
+
+        Char[] SpecialCharacters = "~!@#$%^&*()_+{}|:\"<>?".ToCharArray();
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct INPUT
+        {
+            public INPUTTYPE type;
+            public INPUT_U u;
+
+            public static INPUT VirtualKeyDown(byte keyCode)
+            {
+                var input = new INPUT() { type = INPUTTYPE.Keyboard };
+                input.u.ki = new KEYBDINPUT() { virtualKey = keyCode };
+
+                return input;
+            }
+
+            public static INPUT VirtualKeyUp(byte keyCode)
+            {
+                var input = new INPUT() { type = INPUTTYPE.Keyboard };
+                input.u.ki = new KEYBDINPUT() { virtualKey = keyCode, flags = KEYEVENTF.KeyUp };
+
+                return input;
+            }
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct INPUT_U
+        {
+            [FieldOffset(0)]
+            public KEYBDINPUT ki;
+
+            [FieldOffset(0)]
+            public MOUSEINPUT mi;
+
+            [FieldOffset(0)]
+            public HARDWAREINPUT hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KEYBDINPUT
+        {
+            public ushort virtualKey;
+            public ushort scanCode;
+            public KEYEVENTF flags;
+            public uint time;
+
+            public IntPtr extraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr extraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParam;
+            public short lParam;
+        }
+
+        private enum INPUTTYPE : uint
+        {
+            Keyboard = 1
+        }
+
+        private enum KEYEVENTF : uint
+        {
+            KeyUp = 2,
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            ghk = new GlobalHotkey(Modifiers.CTRL + Modifiers.SHIFT, Keys.Space, Handle);
+        }
+
+        private void HandleHotkey()
+        {
+            int batchSize;
+            try
+            {
+                batchSize = Convert.ToInt32(batchSizeTB.Text);
+            }
+            catch
+            {
+                batchSize = 1000;
+            }
+            int delay;
+            try
+            {
+                delay = Convert.ToInt32(delayTB.Text);
+            }
+            catch
+            {
+                delay = 2000;
+            }
+            String cp = Clipboard.GetText();
+
+            // wait until there are no modifiers
+            while ((Control.ModifierKeys) != 0)
+            {
+                System.Threading.Thread.Sleep(10);
+            }
+            WriteLine("Typed " + cp.Length + " characters.");
+            if (cb_shiftReturn.Checked)
+            {
+                cp = cp.Replace("\r\n", "\n");
+            }
+            Char[] characters = cp.ToCharArray();
+            List<INPUT> inputs = new List<INPUT>();
+            foreach (Char character in characters)
+            {
+                short shorty = VkKeyScan(character);
+                Boolean addShift = checkShift(character, shorty);
+
+                if (cb_shiftReturn.Checked && character == '\n')
+                {
+                    addShift = true;
+                }
+
+                if (addShift)
+                {
+                    inputs.Add(INPUT.VirtualKeyDown((byte)Keys.LShiftKey));
+                }
+                inputs.Add(INPUT.VirtualKeyDown((byte)shorty));
+                inputs.Add(INPUT.VirtualKeyUp((byte)shorty));
+                if (addShift)
+                {
+                    inputs.Add(INPUT.VirtualKeyUp((byte)Keys.LShiftKey));
+                }
+
+                var error = Marshal.GetLastWin32Error();
+            }
+
+            INPUT[] inputsToSend = inputs.ToArray();
+            int i = 0;
+            while (i < inputsToSend.Length)
+            {
+                int newExtent = i + batchSize;
+                if (inputsToSend.Length < newExtent)
+                {
+                    newExtent = inputsToSend.Length;
+                }
+                INPUT[] chunk = new ArraySegment<INPUT>(inputsToSend, i, newExtent - i).ToArray();
+                SendInput(chunk.Length, chunk, Marshal.SizeOf(typeof(INPUT)));
+                i = newExtent;
+                System.Threading.Thread.Sleep(delay);
+            }
+        }
+
+        private Boolean checkShift(Char character, short shorty)
+        {
+            Boolean res = false;
+            string charString = character.ToString();
+            string upper = charString.ToUpper();
+            string lower = charString.ToLower();
+            if (upper == lower)
+            {
+                if (SpecialCharacters.Contains(character))
+                {
+                    res = true;
+                }
+            }
+            else if (charString == upper)
+            {
+                res = true;
+            }
+            return res;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            //windows message id for hotkey
+            int WM_HOTKEY_MSG_ID = 0x0312;
+            if (m.Msg == WM_HOTKEY_MSG_ID)
+            {
+                HandleHotkey();
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WriteLine("Trying to register CTRL+SHIFT+Space");
+            if (ghk.Register())
+            {
+                WriteLine("Hotkey registered.");
+            }
+            else
+            {
+                WriteLine("Hotkey failed to register");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!ghk.Unregiser())
+                MessageBox.Show("Hotkey failed to unregister!");
+        }
+
+        private void WriteLine(string text)
+        {
+            textBox1.Text += text + Environment.NewLine;
+        }
+    }
+}
